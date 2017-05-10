a4f83b151caed228c92226b1b8b54dbb74b54ede
Nohyun Kwak <nhkwak@kaist.ac.kr>
2017-05-10 16:32:59 +0900

    Add rog-in function

--- webServer/routes/index.js
+++ webServer/routes/index.js
@@ -3,7 +3,64 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+
+
+// node-mysql
+var mysql = require('mysql');
+var client = mysql.createConnection({ 
+  host : 'localhost',
+  database : 'sktiotdb',
+  user: 'is521',
+  password : 'is521'
+});
+  
+// running queries as normal...
+client.query('USE shoppingmall');
+
+// mysql-queues
+var queues = require('mysql-queues');
+const DEBUG = true;
+queues(client, DEBUG);
+var q = client.createQueue();
+
+/* login */
+router.post('/login', function(req, res, next) {
+
+    var id = req.param('id'); 
+    var pw = req.param('pw'); 
+
+    // SQL query for checking id, pw 
+    var qString = 'SELECT users.user_id AS id \
+                  FROM users \
+                  WHERE users.user_id = ? AND users.passwd = ?' ; 
+
+    console.log(qString); 
+
+    q.query( qString, [ id, pw ], function( err, result, fields ){
+        if (err) {
+          console.log(err); 
+        }
+        else {
+          	console.log(result);
+          
+            // log-in success 
+          	if (result.length == 1 ) {
+                req.session.regenerate(function(){
+                    console.log(result[0].id);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+                    req.session.user = result[0].id;  
+                    res.redirect('/user');
+                });
+            }
+            else {
+             	res.redirect('/');   
+            }
+    	}
+    }); 
+
+    q.execute(); 
 });
 
 module.exports = router;
