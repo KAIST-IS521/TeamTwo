094e0523cb935f00427cb14a687b812f4ccee2b9
Nohyun Kwak <nhkwak@kaist.ac.kr>
2017-05-10 20:46:20 +0900

    Fix bug because of the depricated parameter format

--- webServer/app.js
+++ webServer/app.js
@@ -18,7 +18,12 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(session({secret: 'keyboard cat'})); 
+app.use( session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  })); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -46,11 +51,11 @@ app.use('/users', users);
 // app.use('/policy', checkAuth, policy);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
