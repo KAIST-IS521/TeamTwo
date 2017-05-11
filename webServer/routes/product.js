b7118db239f001d4007dfc2db7272014e78a9850
Nohyun Kwak <nhkwak@kaist.ac.kr>
2017-05-11 15:37:33 +0900

    Add search function for finding item in product page

--- webServer/routes/product.js
+++ webServer/routes/product.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 
-// DataBase Config 
+// DataBase Config
 // node-mysql
 var mysql = require('mysql');
-var client = mysql.createConnection({ 
+var client = mysql.createConnection({
   host : 'localhost',
   database : 'shoppingmalldb',
   user: 'root',
   password : 'is521'
 });
-  
+
 // running queries as normal...
 client.query('USE shoppingmalldb');
 
@@ -23,53 +23,82 @@ var q = client.createQueue();
 
 
 
-/* 
- * list product list 
+/*
+ * list product list
  */
 router.get('/', function(req, res, next) {
 
   q.query('SELECT * FROM products', function( err, result, fields ){
     if (err) {
-      console.log(err); 
+      console.log(err);
     }
     else {
-      var products = result; 
+      var products = result;
       console.log(result);
-      // client.end(); 
+      // client.end();
 
-      res.render('product', { 'products' : products , 'user' : req.session.user }); 
+      res.render('product', { 'products' : products , 'user' : req.session.user });
     }
-  }); 
+  });
 
   q.execute();
 
 });
 
 
+/*
+ * list product list
+ */
+router.get('/search', function(req, res, next)
+{
+  var keyword = req.param('keyword');
+
+  q.query('SELECT * FROM products WHERE products.name LIKE %?%', [ keyword ], function( err, result, fields ){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      var products = result;
+      console.log(result);
+      // client.end();
+
+      res.render('product', { 'products' : products , 'user' : req.session.user });
+    }
+  });
+
+  q.execute();
+
+});
+
+
+/*
+ * add the item to the shopping cart
+ */
 router.get('/add', function(req, res, next) {
 
 	var id = req.param('id');
 	var num = req.param('num');
 
-    // SQL query for adding 
-    var qString = 'INSERT INTO shopping_cart SET ?'; 
-    var p = { user_id: req.session.user, product_id:id, product_num: 1}; 
+    // SQL query for adding
+    var qString = 'INSERT INTO shopping_cart SET ?';
+    var p = { user_id: req.session.user, product_id:id, product_num: 1};
 
-    console.log(qString); 
+    console.log(qString);
 
     q.query( qString, p, function( err, result, fields ){
         if (err) {
-          console.log(err); 
+          console.log(err);
         }
         else {
             console.log(result);
-            res.json( { message: 'Added to shopping cart...' } );   
+            res.json( { message: 'Added to shopping cart...' } );
         }
-    }); 
+    });
 
     q.execute();
 
 });
+
 
 
 
