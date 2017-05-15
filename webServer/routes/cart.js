9c355195c38124c65377f992505425d733359a10
Nohyun Kwak <nhkwak@kaist.ac.kr>
2017-05-15 20:02:51 +0900

    Add logic for emptying shopping cart

--- webServer/routes/cart.js
+++ webServer/routes/cart.js
@@ -32,4 +32,39 @@ router.get('/', function(req, res, next) {
 
 });
 
+
+/*
+ * purchase all items in the shopping cart
+ */
+router.get('/purchase', function(req, res, next) {
+
+	var qString = 'DELETE shopping_cart  \
+            	FROM ( users JOIN shopping_cart ) JOIN products \
+               	ON users.user_id = shopping_cart.user_id \
+                	AND shopping_cart.product_id = products.product_id\
+                WHERE users.user_id = ? ';
+
+    // bank connection needed
+
+
+    // deleting current items in shopping cart
+	q.query( qString, [ req.session.user ], function( err, result, fields ){
+		if (err) {
+			console.log(err);
+		}
+		else {
+			var items = result;
+			console.log(result);
+
+
+
+			// show empty list in shopping cart
+			res.render('cart', { 'items' : [] , 'user' : req.session.user });
+		}
+	});
+
+	q.execute();
+});
+
+
 module.exports = router;
