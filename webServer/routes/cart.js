67bd6b4de9d828ec3d38d683180337e96daaa219
Nohyun Kwak <nhkwak@kaist.ac.kr>
2017-05-15 21:02:40 +0900

    Move items from shopping_cart table to orders table

--- webServer/routes/cart.js
+++ webServer/routes/cart.js
@@ -36,9 +36,16 @@ router.get('/', function(req, res, next) {
 /*
  * purchase all items in the shopping cart
  */
-router.get('/purchase', function(req, res, next) {
+router.get('/purchase', function(req, res, next)
+{
+	var iString = 'INSERT INTO orders ( user_id, product_id, product_num ) \
+				SELECT users.user_id AS user_id, products.product_id AS product_id, shopping_cart.product_num AS product_num \
+				FROM ( users JOIN shopping_cart ) JOIN products \
+               	ON users.user_id = shopping_cart.user_id \
+                	AND shopping_cart.product_id = products.product_id\
+                WHERE users.user_id = ? ';
 
-	var qString = 'DELETE shopping_cart  \
+	var dString = 'DELETE shopping_cart  \
             	FROM ( users JOIN shopping_cart ) JOIN products \
                	ON users.user_id = shopping_cart.user_id \
                 	AND shopping_cart.product_id = products.product_id\
@@ -48,7 +55,7 @@ router.get('/purchase', function(req, res, next) {
 
 
     // deleting current items in shopping cart
-	q.query( qString, [ req.session.user ], function( err, result, fields ){
+	q.query( iString, [ req.session.user ], function( err, result, fields ){
 		if (err) {
 			console.log(err);
 		}
@@ -56,10 +63,18 @@ router.get('/purchase', function(req, res, next) {
 			var items = result;
 			console.log(result);
 
+			q.query( dString, [ req.session.user ], function( err, result, fields ){
+				if (err) {
+					console.log(err);
+				}
+				else {
+					var items = result;
+					console.log(result);
 
-
-			// show empty list in shopping cart
-			res.render('cart', { 'items' : [] , 'user' : req.session.user });
+					// show empty list in shopping cart
+					res.redirect('/order');
+				}
+			});
 		}
 	});
 
