a7f6cd10885d27e5778aa2443c469c8bfc12c4ac
dinggul <dinggul@kaist.ac.kr>
2017-05-17 16:53:38 +0900

    Add select product's id to div

--- UI/js/cart.js
+++ UI/js/cart.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     $('button.game-item').click(function() {
-        var item_name = $.trim($(this).text().split('\n')[2]);
-        //console.log($('div h3.name-to-remove').text());
+        var item_name = $.trim($(this).text().split('\n')[1]);
         $('div h3.name-to-remove').text(item_name);
+
+        var p_id = $.trim($(this).find('span.product-id').text());
+        $('div span#id-to-send').text(p_id);
+
+        $('div.msgpopup').show();
+    });
+
+    $('button.yesbtn').click(function() {
+        $('div.msgpopup').hide();
+    });
+
+    $('button.nobtn').click(function() {
+        $('div.msgpopup').hide();
     });
 });
