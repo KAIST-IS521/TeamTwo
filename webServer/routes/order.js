c0c804736fe46dd34aef8b160fb93bd5c377757e
Jungsuk Oh <mickan921@gmail.com>
2017-05-17 17:04:29 +0900

    Working on the order view screen

--- webServer/routes/order.js
+++ webServer/routes/order.js
@@ -3,9 +3,50 @@ var router = express.Router();
 var q = require('./db.js');
 
 
-/* GET users listing. */
+/* list ordersre */
 router.get('/', function(req, res, next) {
-  res.send('Sorry... Not integrated yet with Bank system...');
+  q.query('SELECT * FROM orders', function( err, result, fields ){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      var orders = result;
+      console.log(result);
+      // client.end();
+
+      res.render('order', { 'products' : orders , 'user' : req.session.user });
+    }
+  });
+
+  q.execute();
 });
+
+/*
+ * list order list
+ */
+router.get('/search', function(req, res, next)
+{
+    var keyword = req.param('keyword');
+
+    q.query('SELECT * FROM orders WHERE orders.order_id LIKE ?', [ keyword ], function( err, result, fields ){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            var orders = result;
+            console.log(result);
+            // client.end();
+
+            res.render('orders', { 'orders' : orders , 'user' : req.session.user });
+        }
+    });
+
+    q.execute();
+
+});
+
+/*
+TODO: make the add function
+*/
 
 module.exports = router;
