41634b0d31ca23cc7de2d594f386bc895a1e1794
Jungsuk Oh <mickan921@gmail.com>
2017-05-17 17:03:54 +0900

    Made a test order in the database

--- database/insertData.sql
+++ database/insertData.sql
@@ -1,7 +1,7 @@
 INSERT INTO users VALUES( 'test',  'test'); 
 
 
-INSERT INTO products ( price, name ) VALUES( '1000', 'p1' );
+INSERT INTO products ( price, name ) VALUES( '3000', 'p1' );
 INSERT INTO products ( price, name ) VALUES( '2000', 'p2' );
 INSERT INTO products ( price, name ) VALUES( '3000', 'p3' );
 INSERT INTO products ( price, name ) VALUES( '4000', 'p4' );
@@ -10,3 +10,5 @@ INSERT INTO products ( price, name ) VALUES( '6000', 'p6' );
 INSERT INTO products ( price, name ) VALUES( '7000', 'p7' );
 INSERT INTO products ( price, name ) VALUES( '8000', 'p8' );
 INSERT INTO products ( price, name ) VALUES( '9000', 'p9' );
+
+INSERT INTO orders (order_id, user_id, product_id, product_num) VALUES(123, 'test_order', 987, 456);
