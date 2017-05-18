aa706ea092349d4b100cbf5a7cef96db61b2ce5d
Nohyun Kwak <nhkwak@kaist.ac.kr>
2017-05-18 17:28:30 +0900

    Update table and data for PGP-based auth

--- database/insertData.sql
+++ database/insertData.sql
@@ -1,5 +1,34 @@
-INSERT INTO users VALUES( 'test',  'test'); 
+INSERT INTO github_users ( github_id, email ) VALUES('alinghi', 'alinghi@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('asdfljh', 'cjdhlds08@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('bjgwak', 'bjgwak@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('blukat29', 'yunjong@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('DaramG', 'gksgudtjr456@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('dinggul', 'dinggul@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('donghwan17', 'prious@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('ggoboogy', 'jihyeon.yoon@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('Hyeongcheol-An', 'anh1026@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('ian0371', 'ian0371@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('jaemoon-sim', 'jettijam@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('james010kim', 'jangha@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('jcassou', 'jean.cassou-mounat@insa-lyon.fr');
+INSERT INTO github_users ( github_id, email ) VALUES('jchoi2022', 'jschoi.2022@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('JeongOhKye', 'ohkye415@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('jhong3842', 'jhong3842@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('jmpark81', 'jmpark81@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('juanaevv', 'juanaevv@nate.com');
+INSERT INTO github_users ( github_id, email ) VALUES('kaistgun', 'signal@kait.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('lbh0307', 'lbh0307@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('mfaerevaag', 'markus@faerevaag.no');
+INSERT INTO github_users ( github_id, email ) VALUES('mickan921', 'jsoh921@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('mikkang', 'kmb1109@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('nohkwak', 'nhkwak@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('pr0v3rbs', 'pr0v3rbs@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('sbahn1992', 'sbahn1992@gmail.com');
+INSERT INTO github_users ( github_id, email ) VALUES('seongil', 'su3604@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('seungwonwoo', 'seungwonwoo@kaist.ac.kr');
+INSERT INTO github_users ( github_id, email ) VALUES('soomin-kim', 'soomink@kaist.ac.kr');
 
+INSERT INTO users VALUES( 'test',  'test');
 
 INSERT INTO products ( price, name ) VALUES( '1000', 'p1' );
 INSERT INTO products ( price, name ) VALUES( '2000', 'p2' );
