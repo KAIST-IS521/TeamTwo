0ff1821638a96d2a82b6126f48b345ae7d1b93c5
dinggul <dinggul@kaist.ac.kr>
2017-05-19 15:02:59 +0900

    Bug fix on javascript

--- UI/js/search.js
+++ UI/js/search.js
@@ -1,32 +1,3 @@
-$(document).ready(function(){
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip(); 
-  
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-})
-
 // Get the Login modal
 var modal = document.getElementById('modal');
 
@@ -39,18 +10,18 @@ window.onclick = function(event) {
 
 //count product quantity
 function modifyProductQuantity(id, quantity){
-        
+
          if(isNaN($("#"+id).val())){
               alert( 'Only use number' );
               $("#"+id).focus();
               $("#"+id).val(0);
               return;
          }
-        
-         //var v = parseFloat($("#"+id).val())+parseFloat(quantity);    
+
+         //var v = parseFloat($("#"+id).val())+parseFloat(quantity);
          //$("#"+id).val(Math.round(v*10)/10);
-          
-         var q = parseInt($("#"+id).val())+parseInt(quantity);    
+
+         var q = parseInt($("#"+id).val())+parseInt(quantity);
          $("#"+id).val(q);
 
 
