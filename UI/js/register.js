d3a5911379824d7d9584f878af1a5bb8d30f02a4
dinggul <dinggul@kaist.ac.kr>
2017-05-19 21:34:16 +0900

    Porting register.html to register.ejs

--- UI/js/register.js
+++ UI/js/register.js
@@ -1,90 +1,72 @@
- $(document).ready(function(){
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip();
-
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-})
-
-
-function reqFunction() {
-  if(id_git.value.length < 4)
-  {
-    alert("Insert correct Github Account");
-  }
-  else{
-    alert("Request the PGP Key value");
-  }
-  
-  
-}
 function verFunction() {
 
-  //should send value to server 
+    //should send value to server
 
-  var verify = true;
+    var verify = true;
 
-  if(verify == true)
-  {
-    alert("Verified");
-  }
-  else{
-    alert("Insert the correct value again!");
-  }
-  
-  
+    if(verify == true)
+    {
+        alert("Verified");
+    }
+    else{
+        alert("Insert the correct value again!");
+    }
 }
 
-//password check
-$(function(){
-  $('#pw').keyup(function(){
-   $('font[name=check]').text('');
-  }); //#user_pass.keyup
+$(document).ready(function() {
+    $('#random-request').click(function(event) {
+        event.preventDefault();
+        var id_form = $(this).prev();
+        var id_val = id_form.val();
+        $.ajax({url: '/requestPGP',
+                method: 'post',
+                data: {'github-id': id_val},
+                dataType: 'json',
+                success: function(result) {
+                    if (result.status != 1) {
+                        alert('invalid github id, try again :P');
+                    } else {
+                        $('randomvalue').text(result.encrypt);
+                    }
+                }
+        });
+    });
 
-  $('#pw2').keyup(function(){
-   if($('#pw').val()!=$('#pw2').val()){
-    $('font[name=check]').text('');
-    $('font[name=check]').html("Incorrected Password");
-   }else{
-    $('font[name=check]').text('');
-    $('font[name=check]').html("Corrected Password");
-   }
-  }); //#chpass.keyup
- });
+    $('button.signupbtn').click(function(event) {
+        event.preventDefault();
 
-//sign up alert button 
-function signupFunction() {
+        $.ajax({url: '/register',
+                method: 'post',
+                dataType: 'json',
+                data: {'id': $('#id').val(),
+                       'pw': $('#pw').val(),
+                       'github-id': $('#id_git').val(),
+                       'enc-data': $('#response-value').val(),
+                }
+                success: function(result) {
+                    alert(result.message);
+                    if (result.status == 1) {
+                        location.href = '/';
+                    }
+                }
+        });
+    });
 
-  var signup= true;
+    $('button.cancelbtn').click(function(event) {
+        location.href = '/';
+    });
 
-  if(signup == true)
-  {
-    alert("Success");
-  }
-  else{
-    alert("Failed!");
-  }
-  
-  
-}
+    $('#pw').keyup(function(){
+        $('font[name=check]').text('');
+    }); //#user_pass.keyup
+
+    $('#pw2').keyup(function(){
+        if($('#pw').val()!=$('#pw2').val()){
+            $('font[name=check]').text('');
+            $('font[name=check]').html("Incorrected Password");
+        }else{
+            $('font[name=check]').text('');
+            $('font[name=check]').html("Corrected Password");
+        }
+    }); //#chpass.keyup
+});
