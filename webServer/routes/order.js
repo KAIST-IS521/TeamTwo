01bb632aaa2453e1aaa28cd651fa13dca37da085
Jungsuk Oh <mickan921@gmail.com>
2017-05-20 19:30:00 +0900

    Cleaned up order.js

--- webServer/routes/order.js
+++ webServer/routes/order.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var q = require('./db.js');
 
 
-/* list ordersre */
+/* list orders */
 router.get('/', function(req, res, next)
 {
   q.query('SELECT * FROM orders', function( err, result, fields )
@@ -26,36 +26,5 @@ router.get('/', function(req, res, next)
 
   q.execute();
 });
-
-/*
- * list order list
- */
- /*
-router.get('/search', function(req, res, next)
-{
-    var keyword = req.param('keyword');
-
-    q.query('SELECT * FROM orders WHERE orders.order_id LIKE ?', [ keyword ], function( err, result, fields ){
-        if (err)
-        {
-            console.log(err);
-        }
-        else
-        {
-            var orders = result;
-            console.log(result);
-            // client.end();
-
-            res.render('orders', { 'orders' : orders , 'user' : req.session.user });
-        }
-    });
-
-    q.execute();
-
-});
-*/
-/*
-TODO: make the add function
-*/
 
 module.exports = router;
