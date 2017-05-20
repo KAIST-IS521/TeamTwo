0140bf3c3912d1a393be91db09dfc817813a829d
Jungsuk Oh <mickan921@gmail.com>
2017-05-20 16:16:58 +0900

    Updated the orders.ejs, Fixed a bug in insertData.sql

--- database/insertData.sql
+++ database/insertData.sql
@@ -1,7 +1,6 @@
 INSERT INTO users VALUES( 'test',  'test'); 
 
-
-INSERT INTO products ( price, name ) VALUES( '3000', 'p1' );
+INSERT INTO products ( price, name ) VALUES( '3100', 'p1' );
 INSERT INTO products ( price, name ) VALUES( '2000', 'p2' );
 INSERT INTO products ( price, name ) VALUES( '3000', 'p3' );
 INSERT INTO products ( price, name ) VALUES( '4000', 'p4' );
@@ -11,4 +10,4 @@ INSERT INTO products ( price, name ) VALUES( '7000', 'p7' );
 INSERT INTO products ( price, name ) VALUES( '8000', 'p8' );
 INSERT INTO products ( price, name ) VALUES( '9000', 'p9' );
 
-INSERT INTO orders (order_id, user_id, product_id, product_num) VALUES(123, 'test_order', 987, 456);
+INSERT INTO orders (user_id, product_id, product_num) VALUES('test', 2, 5);
