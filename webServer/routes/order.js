5a67a2510a3ff793d975d0d8e2502ea58decfe90
Nohyun Kwak <nhkwak@kaist.ac.kr>
2017-05-23 01:38:24 +0900

    Change API because of adding view button

--- webServer/routes/order.js
+++ webServer/routes/order.js
@@ -6,68 +6,75 @@ var q = require('./db.js');
 var fs = require("fs");
 
 
-/* list orders */
+/*
+ * This function shows order list.
+ */
 router.get('/', function(req, res, next)
 {
-  var qString = 'SELECT orders.order_id AS order_id, products.product_id AS product_id, products.name AS name, products.price AS price, order_items.product_num AS num, orders.status AS status, orders.added_time AS time, orders.bank_account AS bank_account \
+  // query for listing all ordered items
+  var qString = 'SELECT orders.order_id AS order_id, \
+                        products.product_id AS product_id, \
+                        products.name AS name, \
+                        products.price AS price, \
+                        order_items.product_num AS num, \
+                        orders.status AS status, \
+                        orders.added_time AS time, \
+                        orders.bank_account AS bank_account \
                 FROM ( orders JOIN order_items ) JOIN products \
                 ON orders.order_id = order_items.order_id \
                     AND order_items.product_id = products.product_id\
                 WHERE orders.user_id = ?  \
                 ORDER BY orders.added_time DESC';
+
+  // query result handling logic
   q.query(qString, [req.session.user], function( err, result, fields )
-  {
-    if (err)
-    {
-      console.log(err);
-    }
-    else
-    {
-      var orders = result;
-      console.log(result);
-      // client.end();
-
-      res.render('orders', { 'orders' : orders , 'user' : req.session.user });
-    }
-  });
-
-  q.execute();
-
-  //updateUserBankID(2);
-});
-
-function getUserBankID()
-{
-    // bank connected needed
-    return "dummy_bank_id";
-}
-
-function updateUserBankID(order_id)
-{
-  var userBankID = getUserBankID();
-
-  q.query('UPDATE orders SET bank_id = ? WHERE order_id = ?', [userBankID, order_id], function( err, result, fields )
   {
     if (err) {
       console.log(err);
     }
     else {
-      console.log('bank_id update successful');
+      var orders = result;
+      console.log(result);
+
+      // render order.ejs page
+      res.render('order', { 'orders' : orders , 'user' : req.session.user });
     }
   });
 
+  // query execution
   q.execute();
-}
+});
 
-router.get('/requestFlag', function(req, res, next)
+
+/*
+ * This function query the detail information of the ordered item.
+ */
+router.get('/requestDetail', function(req, res, next)
 {
-  var qString = 'SELECT orders.order_id AS order_id, products.product_id AS product_id, products.name AS name, products.price AS price, order_items.product_num AS num, orders.status AS status, orders.added_time AS time, orders.bank_account AS bank_account \
-                FROM ( orders JOIN order_items ) JOIN products \
-                  ON orders.order_id = order_items.order_id \
-                    AND order_items.product_id = products.product_id\
-                  WHERE orders.user_id = ?  AND orders.status = "completed" AND products.name = "FLAG"';
+  // parameter from user
+  var order_id = req.param('order-id');
+  var product_id = req.param('product-id');
 
-  q.query(qString, [req.session.user], function(err, result, fields)
+  // SQL query
+  var qString = 'SELECT orders.order_id AS order_id, \
+                        orders.status AS status, \
+                        orders.added_time AS time, \
+                        orders.bank_account AS bank_account \
+                        order_items.product_num AS num, \
+                        products.product_id AS product_id, \
+                        products.name AS name, \
+                        products.price AS price, \
+                        products.serial_key AS serial_key, \
+                FROM ( orders JOIN order_items ) JOIN products \
+                ON orders.order_id = order_items.order_id \
+                        AND order_items.product_id = products.product_id\
+                WHERE orders.user_id = ?  \
+                        AND orders.order_id = ? \
+                        AND orders.status = "completed" \
+                        AND products.product_id= ? ';
+
+  // query result handling
+  q.query(qString, [ req.session.user, order_id, product_id,  ], function(err, result, fields)
   {
     if(err) {
       console.log(err);
@@ -77,12 +84,11 @@ router.get('/requestFlag', function(req, res, next)
       var orders = result;
       console.log(result);
 
-      if (result.length >= 1)
+      if (result.length == 1 && result[0].name == "FLAG")
       {
         // read encrypted file
         // File path and name to be edited in the final version
-        fs.readFile( './' + 'tmp' + '.asc', function (err, data)
-        {
+        fs.readFile( './' + 'tmp' + '.asc', function (err, data) {
           // file read error
           if (err)
           {
@@ -90,16 +96,22 @@ router.get('/requestFlag', function(req, res, next)
             return  res.json( { 'status' : 0, 'message' : "file error..."});
           }
           console.log("Asynchronous read: " + data.toString());
+
+          result[0].serial_key = data.toString();
           // sending encrypted file
-          res.json( { 'status' : 1 , 'message' : data.toString() });
+          res.json( { 'status' : 1 , 'result' : result[0] });
         });
       }
+      else if ( result.length == 1 ) {
+          return res.json( { 'status' : 1 , 'result' : result[0] });
+      }
       else {
-          return res.json( { 'status' : 0, 'message' : "You didn't buy FLAG item yet..."});
+          return res.json( { 'status' : 0, 'message' : "You didn't buy the item yet..."});
       }
     }
   });
 
+  // query execution
   q.execute();
 });
 
