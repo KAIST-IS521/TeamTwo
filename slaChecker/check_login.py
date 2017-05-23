0a3911ed16289fe7825413abf453d8d70ee10621
dinggul <dinggul@kaist.ac.kr>
2017-05-23 17:35:28 +0900

    Add check_login sla checker functino

--- slaChecker/check_login.py
+++ slaChecker/check_login.py
@@ -1,5 +1,40 @@
 from config import HOST, PORT
 import requests
+import colorlog
+import logging
+import inspect
+
+colorlog.basicConfig(level=logging.INFO)
 
 def check_login():
+    frame = inspect.currentframe()
+    current_function_name = inspect.getframeinfo(frame).function
+
+    try:
+        data = {
+                'id': 'test',
+                'pw': 'test',
+               }
+        r = requests.post('http://{}:{}/login'.format(HOST, PORT),
+                          data=data,
+                          allow_redirects=False)
+
+        if 'set-cookie' not in r.headers:
+            colorlog.error('"{}" failed'.format(current_function_name))
+            return
+
+        cookie = r.headers['set-cookie']
+
+    except:
+        colorlog.error('"{}" failed'.format(current_function_name))
+        return
+
+    colorlog.info('"{}" passed'.format(current_function_name))
+    return cookie
+
+def check_logout(cookie):
     return
+
+if __name__ == '__main__':
+    cookie = check_login()
+    check_logout(cookie)
--- slaChecker/main.py
+++ slaChecker/main.py
@@ -1,5 +1,8 @@
 from check_login import check_login
+from check_connection import check_connection
+
 def main():
+    check_connection()
     check_login()
     return
 
