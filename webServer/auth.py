55aed6f4ecf204117edfbd1dd2c1676210d36c5d
dinggul <dinggul@kaist.ac.kr>
2017-05-23 19:53:50 +0900

    Add script for helping authentication of user

---
+++ webServer/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+
+import os
+from commands import getoutput
+import sys
+
+print 'Paste challenge and press Ctrl-D'
+inp = sys.stdin.read()
+
+res = getoutput('echo "{}" | gpg -d'.format(inp))
+random_number = res.split('\n')[-1].lstrip().rstrip()
+
+res = getoutput('echo "{}" | gpg --encrypt -r "TeamTwo@kaist.ac.kr" --armor --yes')
+
+print
+print
+print
+print
+print
+print
+print
+print res.split('\n', 1)[1]
