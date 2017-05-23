e9d5dc526c9c5d87ff08ae0d9905e99dfe906955
dinggul <dinggul@kaist.ac.kr>
2017-05-23 20:08:12 +0900

    Fix typo

--- webServer/auth.py
+++ webServer/auth.py
@@ -10,7 +10,7 @@ inp = sys.stdin.read()
 res = getoutput('echo "{}" | gpg -d'.format(inp))
 random_number = res.split('\n')[-1].lstrip().rstrip()
 
-res = getoutput('echo "{}" | gpg --encrypt -r "TeamTwo@kaist.ac.kr" --armor --yes')
+res = getoutput('echo "{}" | gpg --encrypt -r "TeamTwo@kaist.ac.kr" --armor --yes'.format(random_number))
 
 print
 print
