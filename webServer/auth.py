90b616366539ee0d503ab7438f6134382fd4ca6c
dinggul <dinggul@kaist.ac.kr>
2017-05-23 20:12:41 +0900

    Now user don't need to type 'yes'

--- webServer/auth.py
+++ webServer/auth.py
@@ -10,7 +10,7 @@ inp = sys.stdin.read()
 res = getoutput('echo "{}" | gpg -d'.format(inp))
 random_number = res.split('\n')[-1].lstrip().rstrip()
 
-res = getoutput('echo "{}" | gpg --encrypt -r "TeamTwo@kaist.ac.kr" --armor --yes'.format(random_number))
+res = getoutput('echo "{}" | gpg --encrypt -r "TeamTwo@kaist.ac.kr" --armor --trust-model always'.format(random_number))
 
 print
 print
