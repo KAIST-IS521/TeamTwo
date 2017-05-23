43848841b8b11987b91389a809e8fb9cf615a827
dinggul <dinggul@kaist.ac.kr>
2017-05-23 21:31:40 +0900

    Add check_search

--- slaChecker/main.py
+++ slaChecker/main.py
@@ -1,10 +1,14 @@
 from check_connection import check_connection
 from check_login import check_login, check_logout
+from check_search import check_search
 
 def main():
     check_connection()
+
     cookie = check_login()
     check_logout(cookie)
+
+    check_search()
     return
 
 if __name__ == '__main__':
