df0e63d1b231275c03c9dcf12c46f73405fdfb13
Markus Faerevaag <m@frv.ag>
2017-05-24 22:35:39 +0900

    Move sla check_auth to login checker

--- slaChecker/main.py
+++ slaChecker/main.py
@@ -1,5 +1,5 @@
 from check_connection import check_connection
-from check_login import check_login, check_logout
+from check_login import check_login, check_logout, check_auth
 from check_search import check_search
 from check_sendmsg import check_sendmsg
 from check_cart import check_cart
@@ -9,6 +9,7 @@ def main():
 
     check_login()
     check_logout()
+    check_auth()
 
     check_search()
 
