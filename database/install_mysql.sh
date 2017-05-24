182b44cbd4fb8509ea60f3b15d39c7058bd66866
dinggul <dinggul@kaist.ac.kr>
2017-05-24 21:21:40 +0900

    Resolve bugs

---
+++ database/install_mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+../gen_root_pw.py
+pw=$(cat ../rootpw)
+echo "mysql-server mysql-server/root_password password ${pw}" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password ${pw}" | sudo debconf-set-selections
+sudo apt-get -y install mysql-server
+mysql -uroot -p${pw} < resetTable.sql
+mysql -uroot -p${pw} < insertData.sql
