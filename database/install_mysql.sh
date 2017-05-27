a4cc75c1caa38cdb82bb203069516a1b9653e2d0
dinggul <dinggul@kaist.ac.kr>
2017-05-27 15:25:23 +0900

    Change mysql install configuration

--- database/install_mysql.sh
+++ database/install_mysql.sh
@@ -2,8 +2,9 @@
 set -x
 ../gen_root_pw.py
 pw=$(cat ../rootpw)
-echo "mysql-server mysql-server/root_password password ${pw}" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password ${pw}" | debconf-set-selections
-apt-get -y install mysql-server
+#echo "mysql-server mysql-server/root_password password ${pw}" | debconf-set-selections
+#echo "mysql-server mysql-server/root_password_again password ${pw}" | debconf-set-selections
+#apt-get -y install mysql-server
+mysqladmin -uroot -p${pw}
 mysql -uroot -p${pw} < resetTable.sql
 mysql -uroot -p${pw} < insertData.sql
